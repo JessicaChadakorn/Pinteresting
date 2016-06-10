36b404b580ee11ca284bbac57585ee8eef2cf61e
Jessica <jess.chad@hotmail.com>
2016-06-10 14:54:56 +1000

    Implemented Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -76,4 +77,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'jess-cat-pinteresting.herokuapp.com', port: 3000 }
 end
